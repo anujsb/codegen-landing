793d11376a486e3174b43756fb3a6b0032859fca
anuj <anujsbhuyar@gmail.com>
2024-07-14 12:29:58 +0530

    init

---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+  <div className="bg-hero h-screen flex justify-center items-center">
+    Hero
+    </div>
+    );
+};
+
+export default Hero;
