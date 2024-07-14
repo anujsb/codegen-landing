1872af0cd654c486534c3b3e0b9ca60087795802
anuj <anujsbhuyar@gmail.com>
2024-07-14 14:16:14 +0530

    hero section grid

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,27 @@
 import React from "react";
+import Image from "next/image";
 
 const Hero = () => {
   return (
-  <div className="bg-hero h-screen flex justify-center items-center">
-    Hero
+    <div className="bg-hero h-screen flex items-center justify-">
+      <div className="grid grid-flow-col grid-cols-2">
+        <div className="flex ">
+          <div className="flex items-center ">
+            backed by
+            {/* <Image
+              src="/ycom.png"
+              width={131.39}
+              height={24.11}
+              alt="Picture of the author m-2 border"
+            /> */}
+          </div>
+          {/* <div>head``</div> */}
+          {/* <div>baki</div> */}
+        </div>
+        <div>image</div>
+      </div>
     </div>
-    );
+  );
 };
 
 export default Hero;
